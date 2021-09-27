133aafb75e8a1859514814d19df088ad65a97bc5
Rob Walters <rob98011@hotmail.com>
2021-09-27 07:55:02 -0400

    fix mysql create table

--- mysqlimg/setup.sql
+++ mysqlimg/setup.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE Stocks;
 USE Stocks;
 
-CREATE TABLE StockData (company_symbol VARCHAR(10), company_name VARCHAR(100),price DECIMAL(6,2), tx_time TIMESTAMP);
+CREATE TABLE StockData (company_symbol VARCHAR(10), company_name VARCHAR(100),price DECIMAL(6,2),Exchange VARCHAR(20), tx_time TIMESTAMP);
 
 CREATE USER 'mysqluser'@'%' IDENTIFIED BY 'pass@word1';
 ALTER USER 'mysqluser'@'%' IDENTIFIED WITH mysql_native_password BY 'pass@word1';
